57a40c49382b9697d1d8d441c5e9568b65d6eff6
Fernando <118523392+arroyofe@users.noreply.github.com>
2026-03-17 20:45:04 +0100

    Actualización desde py2 OK
    La base de datos ya se puede actualizar desde py2

--- py2/app.py
+++ py2/app.py
@@ -2,12 +2,58 @@ import os
 import pymysql
 
 from flask import Flask, jsonify, request
+from flask import render_template, redirect, url_for
 from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
 from db import SessionLocal
 from models import Item
 
 app = Flask(__name__)
+
+# ========== INTERFACE HTML ==========
+
+@app.get("/items/web")
+def web_list_items():
+    with SessionLocal() as s:
+        rows = s.execute(select(Item)).scalars().all()
+        return render_template("list.html", items=rows)
+
+@app.get("/items/web/new")
+def web_new_item_form():
+    return render_template("new.html")
+
+@app.post("/items/web/new")
+def web_new_item_submit():
+    data = request.form
+    with SessionLocal() as s:
+        it = Item(name=data["name"], description=data.get("description"))
+        s.add(it)
+        s.commit()
+        return redirect("/api3" + url_for("web_list_items"))
+
+@app.get("/items/web/<int:item_id>/edit")
+def web_edit_item_form(item_id):
+    with SessionLocal() as s:
+        it = s.get(Item, item_id)
+        return render_template("edit.html", item=it)
+
+@app.post("/items/web/<int:item_id>/edit")
+def web_edit_item_submit(item_id):
+    data = request.form
+    with SessionLocal() as s:
+        it = s.get(Item, item_id)
+        it.name = data.get("name", it.name)
+        it.description = data.get("description", it.description)
+        s.commit()
+        return redirect("/api3" + url_for("web_list_items"))
+
+@app.post("/items/web/<int:item_id>/delete")
+def web_delete_item(item_id):
+    with SessionLocal() as s:
+        it = s.get(Item, item_id)
+        s.delete(it)
+        s.commit()
+        return redirect("/api3" + url_for("web_list_items"))
 
 # --- HEALTHCHECK ---
 @app.get("/health")
